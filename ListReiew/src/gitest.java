3838b706edbfd39f0e7e576c24cdab53e386dbb9
xzt0415 <1634908217@qq.com>
2023-10-14 13:39:51 +0800

    hot-fix commit 1

--- ListReiew/src/gitest.java
+++ ListReiew/src/gitest.java
@@ -8,5 +8,6 @@ public class gitest {
     public static void main(String[] args) {
         System.out.println("hello git ");
         System.out.println("git 2");
+        System.out.println("git 4");
     }
 }
